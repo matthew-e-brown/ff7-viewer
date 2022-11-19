925e74a3b784ae5ee7b097d3afce02e62a27222c
Matthew Brown <matthew.e.brown.17@gmail.com>
2022-11-19 17:34:31 -0500

    Remove wee_alloc
    - We will be doing a TON of allocations, so we take the file-size hit in
      exchange for more performance

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,6 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 
-// Use WeeAlloc instead of Rust's default allocator because it's roughly 10x smaller in size, albeit slower.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     // Make Rust 'panics' log to JS console
